7b538a4299d9dfc3693899ba0927c6a118a450f1
AubreySine0 <94921720+AubreySine0@users.noreply.github.com>
2024-08-15 14:50:02 -0700

    changes

---
+++ scripts/render-markdown.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const { execSync } = require('child_process');
+const os = require('os');
+
+// Get arguments from the command line
+const tpTitle = process.argv[2];
+const jsonSourcePath = process.argv[3];
+const markdownTargetPath = process.argv[4];
+
+// Load the JSON data
+const json = require(jsonSourcePath);
+
+/// CHANGE 1 - rename .data.trackingPlan. to .rules. ///
+const trackEvents = json.rules.events;
+
+let formattedEvents = [];
+let header = tpTitle + '\n';
+formattedEvents.push(header);
+for (let event of trackEvents) {
+    formattedEvent = [];
+    formattedEvent.push('\n');
+
+    formattedEvent.push('### ' + event.name + '\n');
+    eventData = event.rules.properties;
+
+    formattedEvent.push('<!-- tabs:start -->');
+    formattedEvent.push('#### **Basics**' + '\n');
+    formattedEvent.push(event.description);
+    formattedEvent.push('#### **Properties**' + '\n');
+    formattedEvent.push('|**Name** | `Type` | Description | Required?|');
+    formattedEvent.push('| :--- | :--- | :--- | :---|' );
+    for (let propName in eventData.properties.properties) {
+            let propData = eventData.properties;
+            let propType = propData.properties[propName].type;
+            formattedEvent.push('|**' + propName  + '** | `' + propType + '` |...|Required/Optional|' )
+        }
+
+    formattedEvent.push('#### **JS**'+ '\n');
+    formattedEvent.push('```javascript');
+    formattedEvent.push('analytics.track("'+ event.name +'" {')
+
+    for (let propName in eventData.properties.properties) {
+            let propData = eventData.properties;
+            let propType = propData.properties[propName].type;
+            formattedEvent.push('"' + propName  + '": "<<' + propType + '>>"' )
+        }
+    formattedEvent.push('});')
+    formattedEvent.push('```' + ' \n')
+    formattedEvent.push('<!-- tabs:end -->' + '\n');
+    formattedEvent.push('<!-- panels:end -->' + '\n');
+    formattedEvents.push.apply(formattedEvents, formattedEvent);
+}
+
+let eventsMarkdown = formattedEvents.join(os.EOL);
+fs.writeFileSync(markdownTargetPath, eventsMarkdown, 'utf-8');
